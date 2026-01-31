33546f1ba98d272c134589298a9d14e72edba9d9
Felipe <skyer_qx@hotmail.com>
2026-01-30 23:29:38 -0300

    fix casesMobile line breaking

--- src/components/TavWork/CasesMobile.vue
+++ src/components/TavWork/CasesMobile.vue
@@ -61,7 +61,7 @@
                       :ref="(el) => registerTag(el, rIndex, tIndex)"
                       :class="
                         item.hoverBorder +
-                        ' px-3 max-[361px]:px-2 py-1.5 border border-[#2e2e2e] rounded-full text-zinc-300 bg-[#171717] text-[clamp(13px,2.9vw,16px)]'
+                        ' px-3 max-[361px]:px-2 py-1.5 border border-[#2e2e2e] rounded-full text-zinc-300 bg-[#171717] text-[clamp(12px,2.9vw,16px)]'
                       "
                     >
                       {{ tag }}
@@ -70,7 +70,7 @@
                     <span
                       v-if="tIndex < row.length - 1"
                       :ref="(el) => registerDot(el, rIndex, tIndex)"
-                      :class="item.dotColor + ' mt-2'"
+                      :class="item.dotColor + ' mt-2 -mx-0.5'"
                     >
                       ·
                     </span>
@@ -117,7 +117,7 @@
                 >
                   View Case Study
                   <img
-                    src="/src/assets/image/Arrow.png"
+                    src="@/assets/image/Arrow.png"
                     class="ml-3 arrow-black"
                     alt="arrow-black"
                   />
@@ -160,7 +160,7 @@ const updateDots = () => {
     if (!tag || !nextTag || !dot) return;
 
     dot.style.display =
-      tag.offsetTop === nextTag.offsetTop ? "inline-block" : "none";
+      tag.offsetTop === nextTag.offsetTop;
   });
 };
 
